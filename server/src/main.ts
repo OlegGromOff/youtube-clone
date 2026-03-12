d1892fdde8c42a21f0dcca5312675632400b7beb
Oleg Gromov <rekops13@gmail.com>
2026-03-12 16:00:02 +0100

    fix upload limits

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,6 +8,7 @@ import helmet from 'helmet'
 import { AppModule } from './app.module'
 import { PrismaService } from './prisma.service'
 import { IS_DEV_ENV } from './utils/is-dev.util'
+import { json, urlencoded } from 'express'
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule)
@@ -33,9 +34,12 @@ async function bootstrap() {
 
 	if (process.env.FRONTEND_URL) { allowedOrigins.push(process.env.FRONTEND_URL) }
 
-	app.enableCors({ origin: allowedOrigins, credentials: true })
+	// app.enableCors({ origin: allowedOrigins, credentials: true })
+	app.enableCors({ origin: 'https://youtube-clone-eight-tan.vercel.app/', credentials: true, exposedHeaders: 'set-cookie' })
 
 	app.disable('x-powered-by')
+
+	app.use(json({ limit: '100mb' })) app.use(urlencoded({ extended: true, limit: '100mb' }))
 
 	await app.listen(process.env.PORT || 4200, '0.0.0.0')
 }
